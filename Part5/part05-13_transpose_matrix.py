fd964dde0364850d1fcdf3daa2c846cce0113748
A7ryan <136580189+A7ryan@users.noreply.github.com>
2024-06-26 18:10:12 +0530

    Create part05-13_transpose_matrix.py
    Part 5 : Question 13

---
+++ Part5/part05-13_transpose_matrix.py
@@ -0,0 +1,10 @@
+def transpose(matrix: list):
+    for i in range(len(matrix)):
+        for j in range(i+1,len(matrix)):
+            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(matrix)
+    transpose(matrix)
+    print(matrix)
